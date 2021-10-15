19fd1984f7b9174f786cff24ca6be6dc0e2e29f1
m3hrara <mehrarasb@gmail.com>
2021-10-15 03:56:40 -0400

    Added player bullet and direction to bullets

--- GAME2014-Lab4-SarabiMehrara/Assets/[Scripts]/BulletBehaviour.cs
+++ GAME2014-Lab4-SarabiMehrara/Assets/[Scripts]/BulletBehaviour.cs
@@ -8,13 +8,30 @@ public class BulletBehaviour : MonoBehaviour
     [Range(0.0f, 0.5f)]
     public float speed;
     public Bounds bulletBounds;
+    public BulletDirection direction;
 
     private BulletManager bulletManager;
+    private Vector3 bulletVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         bulletManager = GameObject.FindObjectOfType<BulletManager>();
+        switch(direction)
+        {
+            case BulletDirection.UP:
+                bulletVelocity = new Vector3(0.0f, speed, 0.0f);
+                break;
+            case BulletDirection.RIGHT:
+                bulletVelocity = new Vector3(speed, 0.0f, 0.0f);
+                break;
+            case BulletDirection.DOWN:
+                bulletVelocity = new Vector3(0.0f, -speed, 0.0f);
+                break;
+            case BulletDirection.LEFT:
+                bulletVelocity = new Vector3(-speed, 0.0f, 0.0f);
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -26,15 +43,19 @@ public class BulletBehaviour : MonoBehaviour
 
     private void Move()
     {
-        transform.position -= new Vector3(0.0f, speed, 0.0f);
+        transform.position += bulletVelocity;
     }
 
     private void CheckBounds()
     {
+        // checks bottom
         if (transform.position.y < bulletBounds.max)
         {
-            //Destroy(this.gameObject);
-
+            bulletManager.ReturnBullet(this.gameObject);
+        }
+        // checks top
+        if (transform.position.y > bulletBounds.min)
+        {
             bulletManager.ReturnBullet(this.gameObject);
         }
     }
